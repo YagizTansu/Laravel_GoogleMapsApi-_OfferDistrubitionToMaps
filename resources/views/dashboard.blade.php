3fc3ade2b0ac05b7114eb73494a86398748b225e
yagiz <yagiz.tansu99@gmail.com>
2021-07-31 17:04:27 +0300

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">Ship Distribition </x-slot>
+
+    <div class="alert alert-success" role="alert">
+      <h4 class="alert-heading"></h4>
+      <p></p>
+      <p class="mb-0"></p>
+    </div>
+</x-app-layout>
