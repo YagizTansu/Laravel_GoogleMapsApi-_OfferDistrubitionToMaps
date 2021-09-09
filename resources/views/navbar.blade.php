e29d9d1157c92348d7d9aba1e7dfd83257f015d9
yagiz <yagiz.tansu99@gmail.com>
2021-09-09 15:22:03 +0300

    navbar updated

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,37 +1,36 @@
-<nav class="navbar navbar-expand-lg navbar-light " style="background-color: #7cb0d4;">
-    <a href="#" class="navbar-brand">Firm Name</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav class="navbar navbar-expand-lg navbar-light " style="background-color: #5c8aac;">
+    <a href="#" class="navbar-brand">Forwardie</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="/offers">Offers</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/panel/offers">Panel</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/user/profile">Settings</a>
-        </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/logout">Logout</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/try">try</a>
-          </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/offers">Offers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/panel/offers">Panel</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/user/profile">Settings</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/logout">Logout</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
 
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-      </ul>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
     </div>
-  </nav>
+</nav>
